93642118f0fc89dd452ae40005f9a564dae177bb
raxoors <ra@xoors.com>
2017-02-05 16:22:59 +0800

    Initial

---
+++ src/value-objects/commons.js
@@ -0,0 +1,11 @@
+export class Progress {
+  static OPEN = 'O',
+  static DONE = 'D',
+  static CANCELLED = 'C',
+}
+
+export class Status {
+  static ACTIVE = 'A',
+  static ACHIEVED = 'R',
+  static DELETED = 'D',
+}
